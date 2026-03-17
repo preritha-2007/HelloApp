fceda217c0231e4d8aa5f5b913a1f54fd7de5539
preritha-2007 <tp2958@srmist.edu.in>
2026-03-17 13:26:42 +0530

    UC3: Display Hello with default message

--- HelloApp.java
+++ HelloApp.java
@@ -1,15 +1,17 @@
-// UC2 - Display Hello with Command-Line Argument
+// UC3 - Display Hello with Default Name
 
 public class HelloApp {
 
     public static void main(String[] args) {
 
+        String name;
+
         if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello " + name);
+            name = args[0];
         } else {
-            System.out.println("Hello World");
+            name = "World";
         }
 
+        System.out.println("Hello " + name);
     }
 }
