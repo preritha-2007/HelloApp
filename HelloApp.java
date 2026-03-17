3581ba81d0678b0eaa55adf152f991cfd786ff66
preritha-2007 <tp2958@srmist.edu.in>
2026-03-17 13:40:40 +0530

    UC4: Display Hello with multiple names

--- HelloApp.java
+++ HelloApp.java
@@ -1,17 +1,25 @@
-// UC3 - Display Hello with Default Name
+// UC4 - Display Hello with Multiple Names
 
 public class HelloApp {
 
     public static void main(String[] args) {
 
-        String name;
-
         if (args.length > 0) {
-            name = args[0];
-        } else {
-            name = "World";
-        }
 
-        System.out.println("Hello " + name);
+            System.out.print("Hello ");
+
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i]);
+
+                if (i < args.length - 1) {
+                    System.out.print(", ");
+                }
+            }
+
+            System.out.println();
+
+        } else {
+            System.out.println("Hello World");
+        }
     }
 }
