8765491d6893518091abac6d7f21c88006d549e0
preritha-2007 <tp2958@srmist.edu.in>
2026-03-17 13:15:37 +0530

    UC2: Display Hello with Command-Line Argument

--- HelloApp.java
+++ HelloApp.java
@@ -1,10 +1,15 @@
-// UC1 - Display Hello World
+// UC2 - Display Hello with Command-Line Argument
 
 public class HelloApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello " + name);
+        } else {
+            System.out.println("Hello World");
+        }
 
     }
 }
